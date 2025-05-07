13e31bb197b32d5856ee9232fff6ef0f69b48261
ReggieMW <reggiewoods@msn.com>
2025-05-07 11:27:05 -0500

    Code completed comparing distances between all combinations of two TacoBells and returning the two furthest apart from each other.

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -21,24 +21,45 @@ namespace LoggingKata.Test
 
         [Theory]
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
-        //Add additional inline data. Refer to your CSV file.
+        [InlineData("33.470013,-86.816966,Taco Bell Birmingham...", -86.816966)]
+        [InlineData("34.342547,-86.307539,Taco Bell Guntersvill...", -86.307539)]
+        [InlineData("32.637154,-85.41445,Taco Bell Opelik...", -85.41445)]
+        [InlineData("33.796264,-84.224516,Taco Bell Stone Mountain...", -84.224516)]
+        [InlineData("34.113051,-84.56005,Taco Bell Woodstoc...", -84.56005)]
+        
+        //test for correct longitude
         public void ShouldParseLongitude(string line, double expected)
         {
-            // TODO: Complete the test with Arrange, Act, Assert steps below.
-            //       Note: "line" string represents input data we will Parse 
-            //       to extract the Longitude.  
-            //       Each "line" from your .csv file
-            //       represents a TacoBell location
-
             //Arrange
+            var tacoParser = new TacoParser();
 
             //Act
+            string[] splitTacoBell = line.Split(',');
+            var longitude = splitTacoBell[1];
+            var actual = Convert.ToDouble(longitude);
 
             //Assert
+            Assert.Equal(expected, actual);
         }
-
-
-        //TODO: Create a test called ShouldParseLatitude
-
+        
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        [InlineData("33.470013,-86.816966,Taco Bell Birmingham...", 33.470013)]
+        [InlineData("34.342547,-86.307539,Taco Bell Guntersvill...", 34.342547)]
+        [InlineData("32.637154,-85.41445,Taco Bell Opelik...", 32.637154)]
+        [InlineData("33.796264,-84.224516,Taco Bell Stone Mountain...", 33.796264)]
+        [InlineData("34.113051,-84.56005,Taco Bell Woodstoc...", 34.113051)]
+        
+        //test for correct latitude
+        public void ShouldParseLatitude(string line, double expected)
+        {
+            var tacoParser = new TacoParser();
+            
+            string[] splitTacoBell = line.Split(',');
+            var latitude = splitTacoBell[0];
+            var actual = Convert.ToDouble(latitude);
+            
+            Assert.Equal(expected, actual);
+        }
     }
 }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,8 +1,5 @@
 ﻿namespace LoggingKata
 {
-    /// <summary>
-    /// Parses a POI file to locate all the Taco Bells
-    /// </summary>
     public class TacoParser
     {
         readonly ILog logger = new TacoLogger();
@@ -10,43 +7,29 @@
         public ITrackable Parse(string line)
         {
             logger.LogInfo("Begin parsing");
-
-            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
+            
+            //splits string into 3 data points, returning null if less than 3 data points
             var cells = line.Split(',');
-
-            // If your array's Length is less than 3, something went wrong
             if (cells.Length < 3)
             {
-                // Log error message and return null
                 return null; 
             }
+            
+            double latitude = double.Parse(cells[0]);
+            double longitude = double.Parse(cells[1]);
+            string name = cells[2];
+            
+            //using point to create var for physical location to be used in GeoCoordinate
+            Point point = new Point();
+            point.Latitude = latitude;
+            point.Longitude = longitude;
+            
+            //instance to test that string is returned correctly
+            TacoBell tacoBell = new TacoBell();
+            tacoBell.Location = point;
+            tacoBell.Name = name;
 
-            // TODO: Grab the latitude from your array at index 0
-            // You're going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
-            
-            
-            // TODO: Grab the longitude from your array at index 1
-            // You're going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
-            
-            
-            // TODO: Grab the name from your array at index 2
-            
-
-            // TODO: Create a TacoBell class
-            // that conforms to ITrackable
-            
-            // TODO: Create an instance of the Point Struct
-            // TODO: Set the values of the point correctly (Latitude and Longitude) 
-
-            // TODO: Create an instance of the TacoBell class
-            // TODO: Set the values of the class correctly (Name and Location)
-
-            // TODO: Then, return the instance of your TacoBell class,
-            // since it conforms to ITrackable
-
-            return null;
+            return tacoBell;
         }
     }
 }
